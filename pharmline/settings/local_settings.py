af63a83a29c6fc71dec9665676382be91180609a
Qurol Abdujalilov <qurol.abduajalilov99@gmail.com>
2020-12-29 16:43:21 +0500

    allowed host

--- pharmline/settings/local_settings.py
+++ pharmline/settings/local_settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pharmline-uz.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pharmline.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
